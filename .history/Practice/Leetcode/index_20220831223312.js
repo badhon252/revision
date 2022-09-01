763cc6e4e088411d6bede4fb95fded2d65299f2c
khalid <dev.khalidhossain@gmail.com>
2022-09-01 15:55:25 +0600

    test

---
+++ .history/Practice/Leetcode/index_20220831223312.js
@@ -0,0 +1,16 @@
+/ **
+   @param { number [ ] } nums
+ * @param { number } target
+ * @return { number [ ] }
+var twoSum = function ( nums , target ) {
+    let map new Map ( ) ;
+} ;
+    for ( let i = 0 ; i < nums.length ; i ++ ) {
+         let num1 nums [ i ] ;
+         let num2 target
+         if ( map.has ( num2 ) ) {
+                                 num1 ;
+              return [ i , map.get ( num2 ) ] ;
+         }
+    }
+         map.set ( num1 , i ) ;
